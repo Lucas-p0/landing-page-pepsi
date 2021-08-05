b54547fddd2bdf3729ec3d04d796afe7c32c9af7
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-08-05 14:38:51 -0300

    Add code

---
+++ script.js
@@ -0,0 +1,7 @@
+function trocaImg(img) {
+    document.getElementById('img').src = img
+}
+function trocaBackground(cor) {
+    const sect = document.getElementById('sect')
+    sect.style.background = cor
+}
