be04efd476cb400078cd116a3d654bc84651fedd
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-08-09 16:02:04 -0300

    Correções

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function trocaImg(img) {
     document.getElementById('img').src = img
 }
-/*function trocaBackground(cor) {
+function trocaBackground(cor) {
     const sect = document.getElementById('sect')
-    document.body.style.background = cor
-
     sect.style.background = cor
 
-}*/
+}
