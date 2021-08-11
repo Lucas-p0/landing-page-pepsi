b215380b53f3e9584a660bb4608172d0b3518d69
Lucas Pinheiro <61085285+Lucas-p0@users.noreply.github.com>
2021-08-11 15:48:27 -0300

    Add Css

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 function trocaImg(img) {
     document.getElementById('img').src = img
 }
-function trocaBackground(cor) {
-    const sect = document.getElementById('sect')
-    sect.style.backgroundColor = cor
-}
